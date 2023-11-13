077e51c9a083ea141cc39b7e3cf3e485e76006b4
SHNacho <49302304+SHNacho@users.noreply.github.com>
2023-11-13 17:10:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
 
-# try:
-#     import numpy
-# except ImportError:
-#     print('numpy is required during installation')
-#     sys.exit(1)
+try:
+    import numpy
+except ImportError:
+    print('numpy is required during installation')
+    sys.exit(1)
 
-# try:
-#     import scipy
-# except ImportError:
-#     print('scipy is required during installation')
-#     sys.exit(1)
+try:
+    import scipy
+except ImportError:
+    print('scipy is required during installation')
+    sys.exit(1)
 
 setup(name='decision-tree-id3',
       version='0.1.2',
